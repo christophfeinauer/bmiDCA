c0dfbc7e2b3f4c1a295588708b1918f9ea5226e5
christoph <christophfeinauer@gmail.com>
2017-02-14 20:31:44 +0100

    added efu.hpp

---
+++ src/efu.hpp
@@ -0,0 +1,28 @@
+#ifndef EFU_H
+#define EFU_h
+#include "state.hpp"
+#include <vector>
+typedef std::vector<std::vector<std::vector<double> > > tens3;
+typedef std::vector<std::vector<double> > tens2;
+
+
+// pure virtual base class for energy functions
+class Efu{
+	public:
+		virtual double get_energy(State& state) = 0;
+		virtual double get_move_energy_diff(State& state) = 0;
+};
+
+
+class Pairwise : public Efu{
+	tens3 coup;
+	tens2 fields;
+	int len;
+	int q;
+	public:
+		double get_energy(State& state);
+		double get_move_energy_diff(State& state);
+		Pairwise(int, int);
+
+};
+#endif
